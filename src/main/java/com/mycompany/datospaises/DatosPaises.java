9f9bdcebf5ae9ec5f309c643e69f9fa518a27dd1
vsmuggling2023 <144077516+vsmuggling2023@users.noreply.github.com>
2025-09-25 13:32:45 -0300

    PRIMERA VERSION: SOLO VISTA

---
+++ src/main/java/com/mycompany/datospaises/DatosPaises.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datospaises;
+
+/**
+ *
+ * @author Mouli
+ */
+public class DatosPaises {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
